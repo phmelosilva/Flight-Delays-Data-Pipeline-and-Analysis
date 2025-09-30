09b8f789d30fb4172db25d0e6500c8f7518819f3
Matheus Henrique <matheushenriquep285@gmail.com>
2025-09-30 16:00:54 -0300

    feat(ingestion): implementa pipeline de ingestão da Stage para Bronze

---
+++ airflow/dags/ingestion/ingest_stage_to_bronze_dag.py
@@ -0,0 +1,138 @@
+from airflow.decorators import dag, task
+from pendulum import datetime, duration
+from pyspark.sql import SparkSession
+from pipelines.ingestion.check_files_in_stage import check_files_in_stage
+from pipelines.ingestion.spark_processing import reassemble_chunks, save_df_as_single_file
+from pipelines.ingestion.file_management import move_files, delete_files
+
+
+@dag(
+    dag_id="ingest_stage_to_bronze",
+    start_date=datetime(2025, 1, 1, tz="America/Sao_Paulo"),
+    schedule=None,
+    catchup=False,
+    default_args={
+        "depends_on_past": False,
+        "retries": 1,
+        "retry_delay": duration(seconds=30),
+    },
+    tags=["ingestion", "bronze", "stage"],
+    doc_md="""
+    ---------------------------------[ INFO: ]---------------------------------
+
+        DAG de Ingestão da Stage para Bronze
+
+        Esta DAG orquestra a ingestão de dados da área de Stage para a
+        camada Bronze. O processo segue 4 etapas sequenciais:
+        1. Verifica: Verifica a presença dos arquivos CSV na Stage.
+        2. Unifica: Junta os chunks ''flights_part_*.csv' em um único arquivo
+        ''flights.csv' na Stage.
+        3. Move: Transfere os arquivos consolidados para a camada Bronze.
+        4. Limpa: Deleta todos os arquivos da Stage.
+
+    ---------------------------------------------------------------------------
+    """,
+)
+def ingest_stage_to_bronze_dag():
+
+    # Configura o Spark para não criar os checksum nas pastas.
+    spark_conf = {
+        "spark.hadoop.fs.file.impl": "org.apache.hadoop.fs.RawLocalFileSystem"
+    }
+
+    @task
+    def check_files_in_stage_task(stage_path: str) -> list[str]:
+        """
+        Verifica se há arquivos CSV disponíveis na Stage.
+        """
+        return check_files_in_stage(stage_path=stage_path, file_pattern="*.csv")
+
+    @task
+    def unify_chunks_task(files_in_stage: list[str], stage_path: str, spark_cores: str) -> str:
+        """
+        Unifica os arquivos de chunk (`flights_part_*.csv`) em um único `flights.csv`.
+        """
+        spark = SparkSession.builder \
+            .appName("UnifyFlightsChunks") \
+            .master(f"local[{spark_cores}]") \
+            .config(map=spark_conf) \
+            .getOrCreate()
+            
+        try:
+            flight_chunks = [f for f in files_in_stage if "flights_part" in f]
+            flights_df = reassemble_chunks(spark, flight_chunks)
+            
+            new_filename = "flights.csv"
+            save_df_as_single_file(flights_df, dest_path=stage_path, filename=new_filename)
+
+            return f"{stage_path}/{new_filename}"
+        
+        finally:
+            spark.stop()
+
+    @task
+    def move_files_to_bronze_task(initial_files: list[str], unified_file: str, bronze_path: str, processing_date: str):
+        """
+        Move os arquivos finais da Stage para a camada Bronze.
+        """
+        spark = SparkSession.builder \
+            .appName("MoveToBronze") \
+            .master("local[1]") \
+            .config(map=spark_conf) \
+            .getOrCreate()
+        try:
+            files_to_move = [f for f in initial_files if "flights_part" not in f]
+            files_to_move.append(unified_file)
+
+            move_files(
+                spark=spark,
+                source_files=files_to_move,
+                base_dest_path=bronze_path,
+                processing_date=processing_date
+            )
+
+        finally:
+            spark.stop()
+
+    @task
+    def clean_up_stage_task(initial_files: list[str], unified_file: str):
+        """
+        Remove todos os arquivos da Stage após a movimentação para Bronze.
+        """
+        spark = SparkSession.builder \
+            .appName("CleanUpStage") \
+            .master("local[1]") \
+            .config(map=spark_conf) \
+            .getOrCreate()
+        try:
+            files_to_delete = initial_files + [unified_file]
+            delete_files(spark, files_to_delete)
+
+        finally:
+            spark.stop()
+
+    initial_file_list = check_files_in_stage_task(
+        stage_path="{{ var.value.datalake_stage_path }}"
+    )
+    
+    unified_file_path = unify_chunks_task(
+        files_in_stage=initial_file_list,
+        stage_path="{{ var.value.datalake_stage_path }}",
+        spark_cores="{{ var.value.airflow_spark_cores | default('4') }}"
+    )
+    
+    move_task_instance = move_files_to_bronze_task(
+        initial_files=initial_file_list,
+        unified_file=unified_file_path,
+        bronze_path="{{ var.value.datalake_bronze_path }}",
+        processing_date="{{ ds }}"
+    )
+    
+    cleanup_task_instance = clean_up_stage_task(
+        initial_files=initial_file_list, 
+        unified_file=unified_file_path
+    )
+
+    unified_file_path >> move_task_instance >> cleanup_task_instance
+
+ingest_stage_to_bronze_dag()
---
+++ pipelines/ingestion/file_management.py
@@ -0,0 +1,94 @@
+from pyspark.sql import SparkSession
+from airflow.utils.log.logging_mixin import LoggingMixin
+from pathlib import Path as PythonPath
+from collections import defaultdict
+
+
+log = LoggingMixin().log
+
+def move_files(spark: SparkSession, source_files: list[str], base_dest_path: str, processing_date: str) -> None:
+    """
+    Move arquivos de uma área para outra.
+
+    Args:
+        spark (SparkSession): Sessão Spark ativa.
+        source_files (list[str]): Lista de caminhos completos dos arquivos de origem.
+        base_dest_path (str): Caminho base do diretório de destino.
+        source_name (str): Nome da fonte de dados (ex: 'csv_files', 'flight_data').
+        processing_date (str): A data de processamento para criar a partição.
+
+    Raises:
+        ValueError: Se a lista de arquivos estiver vazia.
+        IOError: Se algum arquivo não puder ser movido.
+    """
+    if not source_files:
+        raise ValueError("Nenhum caminho de arquivo fornecido.")
+
+    fs = spark._jvm.org.apache.hadoop.fs.FileSystem.get(spark._jsc.hadoopConfiguration())
+    Path = spark._jvm.org.apache.hadoop.fs.Path
+
+    # Agrupando os arquivos por tipo.
+    destinations = defaultdict(list)
+    for file_path in source_files:
+        py_path = PythonPath(file_path)
+        file_type = py_path.suffix.lstrip('.').upper()
+
+        if not file_type:
+            log.info(f"Arquivo {file_type} não possui extensão.")
+            continue
+
+        dest_dir = f"{base_dest_path}/{processing_date}/{file_type}"
+        destinations[dest_dir].append(file_path)
+    
+    # Criando os diretórios de destino.
+    for dest_dir_str in destinations.keys():
+        dest_path = Path(dest_dir_str)
+        if not fs.exists(dest_path):
+            fs.mkdirs(dest_path)
+
+    # Movendo os arquivos.
+    log.info(f"Movendo arquivos para: {base_dest_path}")
+    for dest_dir_str, files_to_move in destinations.items():
+        dest_path = Path(dest_dir_str)
+        for file_path in files_to_move:
+            source_path = Path(file_path)
+            if fs.rename(source_path, dest_path):
+                log.info(f"Arquivo {source_path.getName()} movido com sucesso.")
+            else:
+                raise IOError(f"Não foi possível mover '{file_path}' para '{dest_path}'.")
+
+def delete_files(spark: SparkSession, files_to_delete: list[str]) -> None:
+    """
+    Deleta arquivos de um determinado local.
+
+    Args:
+        spark (SparkSession): Sessão Spark ativa.
+        files_to_delete (list[str]): Lista de caminhos dos arquivos a serem deletados.
+
+    Raises:
+        IOError: Se algum arquivo não puder deletado.
+        Exception: Se ocorrer qualquer falha inesperada na operação com o sistema de arquivos.
+    """
+    if not files_to_delete:
+        log.info("Nenhum arquivo para deletar.")
+        return
+    
+    fs = spark._jvm.org.apache.hadoop.fs.FileSystem.get(spark._jsc.hadoopConfiguration())
+    Path = spark._jvm.org.apache.hadoop.fs.Path
+
+    for file_path in files_to_delete:
+        path_to_delete = Path(file_path)
+
+        try:
+            if fs.exists(path_to_delete):
+                if fs.delete(path_to_delete, True):
+                    log.info(f"Arquivo '{file_path}' deletado com sucesso.")
+                else:
+                    raise IOError(f"Falha ao deletar arquivo '{file_path}'.")
+            else:
+                log.warning(f"Arquivo '{file_path}' não encontrado. Ignorando.")
+        except Exception as e:
+            log.error(f"Erro ao deletar '{file_path}': {e}")
+            raise
+
+    log.info("Limpeza concluída.")
