c04698c9160aba1658e07c28e6bc7a1990f270e4
JoaoSchmitz <jmatheus12349@gmail.com>
2025-11-06 18:41:24 -0300

    feat: add criação de tabelas da camada Gold

---
+++ postgres/init/002_create_table_gold.sql
@@ -0,0 +1,116 @@
+-- ---------------------------------------------------------------------------------------------------------------------
+--
+--                                        SCRIPT DE CRIAÇÃO (DDL)                                                
+-- 
+-- Data Criação ...........: 04/11/2025
+-- Autor(es) ..............: Matheus Henrique Dos Santos, Joao Matheus de Oliveira Schmitz
+-- Banco de Dados .........: PostgreSQL 16
+-- Banco de Dados(nome) ...: dw
+--
+-- PROJETO => 03 Base de Dados
+--         => 04 Tabelas
+--
+-- ---------------------------------------------------------------------------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS gold;
+SET search_path TO gold;
+
+-- Tabelas --
+CREATE TABLE IF NOT EXISTS dim_airport (
+    airport_id SERIAL,
+    airport_iata_code VARCHAR(3) UNIQUE NOT NULL,
+    airport_name VARCHAR(100) NOT NULL,
+    
+    state_code VARCHAR(3) NOT NULL,
+    state_name VARCHAR(100),
+
+    city_name VARCHAR(100) NOT NULL,
+
+    latitude DOUBLE PRECISION,
+    longitude DOUBLE PRECISION
+);
+
+CREATE TABLE IF NOT EXISTS dim_airline (
+    airline_id SERIAL,
+    airline_iata_code VARCHAR(3) UNIQUE NOT NULL,
+    airline_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS dim_date (
+    date_id SERIAL,
+    full_date DATE UNIQUE NOT NULL,
+    year SMALLINT NOT NULL,
+    month SMALLINT NOT NULL,
+    day SMALLINT NOT NULL,
+    day_of_week SMALLINT NOT NULL,
+    quarter SMALLINT,
+    is_holiday BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE IF NOT EXISTS fato_flights (
+    flight_id INTEGER,
+    date_id INTEGER NOT NULL,
+    airline_id INTEGER NOT NULL,
+    origin_airport_id INTEGER NOT NULL,
+    dest_airport_id INTEGER NOT NULL,
+
+    scheduled_departure DOUBLE PRECISION,
+    departure_time DOUBLE PRECISION,
+    scheduled_arrival DOUBLE PRECISION,
+    arrival_time DOUBLE PRECISION,
+    wheels_off DOUBLE PRECISION,
+    wheels_on DOUBLE PRECISION,
+
+    distance DOUBLE PRECISION,
+    air_time DOUBLE PRECISION,
+    elapsed_time DOUBLE PRECISION,
+    scheduled_time DOUBLE PRECISION,
+    taxi_out DOUBLE PRECISION,
+    taxi_in DOUBLE PRECISION,
+    departure_delay DOUBLE PRECISION,
+    arrival_delay DOUBLE PRECISION,
+
+    air_system_delay DOUBLE PRECISION DEFAULT 0,
+    security_delay DOUBLE PRECISION DEFAULT 0,
+    airline_delay DOUBLE PRECISION DEFAULT 0,
+    late_aircraft_delay DOUBLE PRECISION DEFAULT 0,
+    weather_delay DOUBLE PRECISION DEFAULT 0
+);
+
+-- Chaves Primárias (PKs) --
+ALTER TABLE dim_airport ADD CONSTRAINT pk_dim_airport PRIMARY KEY (airport_id);
+ALTER TABLE dim_airline ADD CONSTRAINT pk_dim_airline PRIMARY KEY (airline_id);
+ALTER TABLE dim_date ADD CONSTRAINT pk_dim_date PRIMARY KEY (date_id);
+ALTER TABLE fato_flights ADD CONSTRAINT pk_fato_flights PRIMARY KEY (flight_id);
+
+-- Chaves Estrangeiras (FKs) --
+ALTER TABLE fato_flights ADD CONSTRAINT fk_fato_flights_date_id
+    FOREIGN KEY (date_id) REFERENCES dim_date(date_id);
+
+ALTER TABLE fato_flights ADD CONSTRAINT fk_fato_flights_airline_id
+    FOREIGN KEY (airline_id) REFERENCES dim_airline(airline_id);
+
+ALTER TABLE fato_flights ADD CONSTRAINT fk_fato_flights_origin_airport_id
+    FOREIGN KEY (origin_airport_id) REFERENCES dim_airport(airport_id);
+
+ALTER TABLE fato_flights ADD CONSTRAINT fk_fato_flights_dest_airport_id
+    FOREIGN KEY (dest_airport_id) REFERENCES dim_airport(airport_id);
+
+
+-- Índices para acelerar os JOINs da Fato com as Dimensões --
+CREATE INDEX IF NOT EXISTS idx_flights_date ON fato_flights(date_id);
+CREATE INDEX IF NOT EXISTS idx_flights_airline ON fato_flights(airline_id);
+CREATE INDEX IF NOT EXISTS idx_flights_origin ON fato_flights(origin_airport_id);
+CREATE INDEX IF NOT EXISTS idx_flights_dest ON fato_flights(dest_airport_id);
+
+-- Índices para acelerar métricas e Filtros na tabela Fato --
+CREATE INDEX IF NOT EXISTS idx_fato_flights_arrival_delay ON fato_flights (arrival_delay);
+CREATE INDEX IF NOT EXISTS idx_fato_flights_scheduled_departure ON fato_flights (scheduled_departure);
+CREATE INDEX IF NOT EXISTS idx_fato_flights_scheduled_time ON fato_flights (scheduled_time);
+
+-- Índices para acelerar os Filtros nas Dimensões --
+CREATE INDEX IF NOT EXISTS idx_dim_airline_airline_name ON dim_airline (airline_name);
+CREATE INDEX IF NOT EXISTS idx_dim_airport_airport_name ON dim_airport (airport_name);
+CREATE INDEX IF NOT EXISTS idx_dim_airport_state_code ON dim_airport (state_code);
+
+
+COMMENT ON SCHEMA gold IS 'Modelagem Star, otimizada para BI e IA.';
